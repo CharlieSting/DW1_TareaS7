0137baca77be15b046294609c9c68cceb7011ec3
CharlieSting <101151950+CharlieSting@users.noreply.github.com>
2023-03-20 17:06:23 -0600

    comit

--- Login_s7/Login_s7/Login_s7.Android/MainActivity.cs
+++ Login_s7/Login_s7/Login_s7.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace Login_s7.Droid
 {
-    [Activity(Label = "Login_s7", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Login_s7", Icon = "@mipmap/iconticket", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
